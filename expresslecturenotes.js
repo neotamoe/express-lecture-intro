58016fb0cc4c1ca2a8918f7eda6e0ad83ec259c9
Neota Moe <neota.moe@gmail.com>
2017-04-20 10:20:15 -0500

    initial commit

---
+++ expresslecturenotes.js
@@ -0,0 +1,21 @@
+// node allows us to write server code
+// node allows us to run javascript files from terminal
+// express uses routes and so listening is done easier
+// framework for node.js, an easier way to write in node, cleaner syntax
+// expressjs.com
+//
+
+// brew is a package manager for macOS (we used it to install node)
+// node has its own package manager called npm
+// we will use npm in terminal to access the package manager
+// make sure folder names do not have spaces or camelCase
+
+// 1) when you first start node project, must be in your project folder and then run $ npm init --yes
+// this turns the directory into a node project and creates a json package
+// 2) then in terminal run $npm install express --save (still in your folder)
+// this creates dependencies which says which version of express you're using AND adds the node_modules folder
+// 3) make file called .gitignore
+
+/*
+
+*/
